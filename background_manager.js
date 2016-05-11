ad6118250494b0a1d2c7bdcf92e0b02e29e7bb2e
Amal Jose <amaljose96@gmail.com>
2016-05-11 12:25:12 +0300

    Completed Home Page Design
    Completed the HOME page.
    Only FB login left

--- background_manager.js
+++ background_manager.js
@@ -6,7 +6,7 @@ function random_movements(){
   var no_of_bands=height/100;
   console.log("Height is "+height);
   console.log("Number of Band="+no_of_bands);
-  for(var i=0;i<no_of_bands-1;i++){
+  for(var i=0;i<no_of_bands;i++){
     var random_dir=Math.floor(Math.random()*2);
     var random_left=Math.floor(Math.random()*client_window_height);
     var random_font_size=Math.floor(Math.random()*9000)/100;
@@ -22,43 +22,76 @@ function random_movements(){
     else{
       random_text="tathva '16";
     }
-    console.log(i+":dir="+random_dir+"||left="+random_left+"||font_size="+random_font_size);
+    //console.log(i+":dir="+random_dir+"||left="+random_left+"||font_size="+random_font_size);
     var band=$("<div id='random_bar_"+i+"' class='random_bar' speed="+random_speed+" dir="+random_dir+" style='margin-left:"+random_left+"px; height='"+random_height+"px'>"+random_text+"</div>");
     $('#background').append(band);
     document.getElementById("random_bar_"+i).style.fontSize=random_font_size+"px";
     document.getElementById("random_bar_"+i).style.marginBottom=random_height+"px";
   }
-  for(var i=0;i<10;i++){
-  setInterval(function(){
-    continue_movements();
-  },1000/60);
-}
+    setInterval(function(){
+      continue_movements();
+    },1000/60);
+
 }
 function continue_movements(){
-  console.log("MOVING");
+  //console.log("MOVING");
   var background=$('#background');
   var height=background.height();
   var no_of_bands=height/100;
-  for(var i=0;i<no_of_bands-1;i++){
+  for(var i=0;i<no_of_bands;i++){
     var dir=$("#random_bar_"+i).attr("dir");
-    var spd=$("#random_bar_"+i).attr("speed");
+    var spd=Math.floor(2/Math.floor(document.getElementById("random_bar_"+i).style.fontSize.replace("px","")/20));
+    if(spd==0){
+      spd=1;
+    }
     var cur_left=document.getElementById("random_bar_"+i).style.marginLeft;
-    console.log("Got cur_left as "+cur_left);
+//    console.log("Got cur_left as "+cur_left);
     cur_left=parseInt(cur_left);
-    console.log("Read spd:"+spd+" and dir:"+dir+" and "+cur_left);
+  //  console.log("Read spd:"+spd+" and dir:"+dir+" and "+cur_left);
     if(dir==0){
-      cur_left+=1;
+      cur_left+=spd;
       if(cur_left>1500){
+        var random_font_size=Math.floor(Math.random()*9000)/100;
+        var random_height=100-random_font_size;
+        document.getElementById("random_bar_"+i).style.fontSize=random_font_size+"px";
+        document.getElementById("random_bar_"+i).style.marginBottom=random_height+"px";
         cur_left=$("#random_bar_"+i).width()*-1+Math.floor(Math.random()*900);
       }
     }
     else{
-      cur_left-=1;
+      cur_left-=spd;
       if(cur_left<-1000){
+
+        var random_font_size=Math.floor(Math.random()*9000)/100;
+        var random_height=100-random_font_size;
+        document.getElementById("random_bar_"+i).style.fontSize=random_font_size+"px";
+        document.getElementById("random_bar_"+i).style.marginBottom=random_height+"px";
         cur_left=1500+Math.floor(Math.random()*900);
       }
     }
-    console.log("CHANGING "+i+":"+cur_left);
+  //  console.log("CHANGING "+i+":"+cur_left);
       document.getElementById("random_bar_"+i).style.marginLeft=cur_left+"px";
   }
+}
+function move_background_up(){
+  var filterVal = 'blur(4px)';
+  $('#background').css('filter',filterVal).css('webkitFilter',filterVal).css('mozFilter',filterVal).css('oFilter',filterVal).css('msFilter',filterVal) .css('transition', 'all 2s ease-out').css('-webkit-transition', 'all 2s ease-out').css('-moz-transition', 'all 2s ease-out').css('-o-transition', 'all 2s ease-out');
+  $( "#background" ).animate({
+     opacity: 1,
+     top: "-50px",
+     filter: filterVal
+   }, 1000, function() {
+     toggle=1;
+   });
+}
+function move_background_down(){
+  var filterVal = 'blur(0px)';
+  $('#background').css('filter',filterVal).css('webkitFilter',filterVal).css('mozFilter',filterVal).css('oFilter',filterVal).css('msFilter',filterVal) .css('transition', 'all 2s ease-out').css('-webkit-transition', 'all 2s ease-out').css('-moz-transition', 'all 2s ease-out').css('-o-transition', 'all 2s ease-out');
+  $( "#background" ).animate({
+     opacity: 1,
+     top: "0px"
+   }, 1000, function() {
+     toggle=0;
+
+   });
 }
--- core.js
+++ core.js
@@ -8,14 +8,57 @@ function clean_HOME(){
   document.getElementById("tathva_man").style.height=client_window_height/7;
   document.getElementById("tathva").style.left=client_window_width/30;
   document.getElementById("cpr").style.left=client_window_width/2.7;
-
+  document.getElementById("HOME_2").style.top=client_window_height;
+  document.getElementById("HOME2_Text").style.fontSize=client_window_height/10;
+  document.getElementById("HOME2_Text2").style.fontSize=client_window_height/15;
+  document.getElementById("HOME2_Text").style.marginLeft=client_window_width/10;
+  document.getElementById("HOME2_Text2").style.marginLeft=client_window_width/6;
+  document.getElementById("HOME2_Text2").style.marginTop=client_window_height/15;
+  document.getElementById("HOME2_Text2").style.marginBottom=client_window_height/15;
+  document.getElementById("fb_button").style.marginLeft=client_window_width*2/3;
 }
 function call_HOME(){
   //EXCECuTE PHP
   clean_HOME();
 }
+var toggle=0;
 function toggle_HOME_2(){
-
+  if(toggle==0){
+    move_background_up();
+    $("#Heading").animate({
+          marginTop:"-500px"
+    },2000,function(){});
+    $( "#tathva" ).animate({
+       left: (((client_window_width*24)/30)-10)+"px"
+     }, 2000, function() {
+    });
+    $( "#tathva_man" ).animate({
+       left: (((client_window_width*23)/30)-10)+"px"
+     }, 2000, function() {
+    });
+    $( "#HOME_2" ).animate({
+       top: "100px"
+     }, 2000, function() {
+    });
+  }
+  if(toggle==1){
+    move_background_down();
+    $("#Heading").animate({
+        marginTop:(client_window_height/8)+"px"
+    },2000,function(){});
+    $( "#tathva" ).animate({
+       left: client_window_width/30+"px"
+     }, 2000, function() {
+    });
+    $( "#tathva_man" ).animate({
+       left: client_window_width/40+"px"
+     }, 2000, function() {
+    });
+    $( "#HOME_2" ).animate({
+       top: client_window_height+"px"
+     }, 2000, function() {
+    });
+  }
 }
 $("document").ready(function(){
   random_movements();
