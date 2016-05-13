7afec570768def0c78e2b9da946815957dee8d84
Amal Jose <amaljose96@gmail.com>
2016-05-14 00:33:56 +0300

    Brain framework completely working

--- Brain files/brain.php
+++ Brain files/brain.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 /*
 CLUELESS '16 Engine Built for Tathva '16
 ----------------------------------------
@@ -44,7 +45,7 @@ QUESTION MODE:
 This is called only from Question page.
 Parameter- ANS
 
-The session Id is used to get current level.
+The User Database is used to get current level.
 Current level answer is retrieved and checked with provided answer.
 If same, the current level is updated and new question is loaded.
 Else, previous question is loaded.
@@ -82,8 +83,13 @@ For now, lets use following details for user.
 
   From here the password is checked. If yes, load QUESTION page. Else load same login page.
 
+Apart from this an error channel is also used. The ERROR_CODE parameter displays error.Default=-1
 
-
+1-Invalid User/Password
+2-Duplicate User
+3-Passwords Dont Match
+4-Taunt_Success
+5-Taunt_Fail
 BRAIN MAP:
 
                                       -----------------------------
@@ -102,53 +108,495 @@ HOME------>Login-----Success------T---->QUESTION-----AnswerCheck--|
 
 
 //LETS START oFF
-//DATABASE DETAILS
-$username = "root";
-$password='Amaljose@96';
-$database= "test";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-// Check connection
-if ($conn->connect_error)
-{
-   die("Connection failed: " . $conn->connect_error);
-}
-
 //RETRIVE opcode
 
 $op_code=$_POST["OPCODE"];
 if($op_code==1){                    ////////////TIME FOR DIRECT LINK
-  $destination=$_POST["DEST"];
-  if($destination==1){                ///////WARP TO HOME
-    $script="<script>
-      function trigger_login(){
-        var dataString='OP_CODE=1&DEST=7';
-        $.ajax({
-            type: \"POST\",
-            url: \"brain.php\",
-            data: dataString,
-            cache: false,
-            success: function(result){
-              $('#content').html(result);
-              }
-          });
-      }
-        </script>";
-    $html="<div>CLUELESS '16</div>
+    $destination=$_POST["DEST"];
+    if($destination==1){                ///////WARP TO HOME
+      //FIRST LOGOUT
+      if(session_id()){
+      session_unset();
+      session_destroy();
+    }
+      echo "<script>
+            function trigger_login(){
+            var dataString='OPCODE=1&ERROR_CODE=0&DEST=7';
+            load(dataString);
+            }
+            </script>";
+      echo "<div>CLUELESS '16</div>
           <div>Welcome</div>
           <div>Gotham awaits your call</div>
           <div id='button' onclick='trigger_login()'>Log In</div>
           ";
-        echo $script;
-        echo $html;
+    }
+    else if($destination==2){
+      echo "<script>
+            function check_answer(){
+            var answer=$('#answer_box').text();
+            var dataString='OPCODE=2&ERROR_CODE=0&ANS='+answer;
+            load(dataString);
+            }
+            function jump_to_question(){
+            load('OPCODE=1&ERROR_CODE=0&DEST=2');
+            }
+            function jump_to_highscore_A(){
+              load('OPCODE=1&ERROR_CODE=0&DEST=5');
+            }
+            function jump_to_highscore_H(){
+              load('OPCODE=1&ERROR_CODE=0&DEST=6');
+                }
+            function jump_to_rules(){
+                  load('OPCODE=1&ERROR_CODE=0&DEST=3');
+                }
+             function jump_to_profile(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=4');
+              }
+             </script>";
+        $level=cur_user_cur_level(); //OBTAIN FROM DATABASE
+        echo "
+            <img src='";
+        $img_name=get_image_name($level);
+        echo $img_name;
+        echo ".jpeg'>
+            <div id='answer_box' contenteditable='true'>Answer here</div>
+            <div id='Submit_button' onclick='check_answer()'>Submit</div>
+            <div id='links'>
+            <div id='link0' onclick='jump_to_question()'>Game</div>
+            <div id='link1' onclick='jump_to_profile()'>Profile</div>
+            <div id='link2' onclick='jump_to_rules()'>Rules</div>
+            <div id='link3' onclick='jump_to_highscore_A()'>Highscore-Away</div>
+            <div id='link4' onclick='jump_to_highscore_H()'>Highscore-Home</div>
+            </div>
+            ";
+      }
+      else if($destination==3){
+        echo "<script>
+              function jump_to_question(){
+                  load('OPCODE=1&ERROR_CODE=0&DEST=2');
+              }
+              function jump_to_highscore_A(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=5');
+              }
+              function jump_to_highscore_H(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=6');
+              }
+                function jump_to_rules(){
+                  load('OPCODE=1&ERROR_CODE=0&DEST=3');
+                }
+                function jump_to_profile(){
+                  load('OPCODE=1&ERROR_CODE=0&DEST=4');
+                }
+                </script>
+                ";
+         echo "
+              <div id='Rules_heading'>Rules:</div>
+              <div>1.Hello</div>
+              <div>2.How are you?</div>
+              <div id='links'>
+                <div id='link0' onclick='jump_to_question()'>Game</div>
+                <div id='link1' onclick='jump_to_profile()'>Profile</div>
+                <div id='link2' onclick='jump_to_rules()'>Rules</div>
+                <div id='link3' onclick='jump_to_highscore_A()'>Highscore-Away</div>
+                <div id='link4' onclick='jump_to_highscore_H()'>Highscore-Home</div>
+              </div>
+              ";
+       }
+       else if($destination==4){
+         echo "<script>
+              function jump_to_question(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=2');
+              }
+              function jump_to_highscore_A(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=5');
+              }
+              function jump_to_highscore_H(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=6');
+              }
+              function jump_to_rules(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=3');
+              }
+              function jump_to_profile(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=4');
+              }
+              </script>
+              ";
+          echo "
+              <div id='Rules_heading'>Profile:</div>
+              <div>Name:";
+          echo cur_user_name();
+          echo "</div>
+              <div>Level:";
+          echo cur_user_cur_level();
+          echo "</div>
+                <div>College:";
+          echo cur_user_college();
+          echo "</div>
+                <div id='links'>
+                <div id='link0' onclick='jump_to_question()'>Game</div>
+                <div id='link1' onclick='jump_to_profile()'>Profile</div>
+                <div id='link2' onclick='jump_to_rules()'>Rules</div>
+                <div id='link3' onclick='jump_to_highscore_A()'>Highscore-Away</div>
+                <div id='link4' onclick='jump_to_highscore_H()'>Highscore-Home</div>
+                </div>
+                ";
+        }
+        else if($destination==5){             //Highscore-Away
+          echo "<script>
+                function jump_to_question(){
+                load('OPCODE=1&ERROR_CODE=0&DEST=2');
+                }
+                function jump_to_highscore_A(){
+                  load('OPCODE=1&ERROR_CODE=0&DEST=5');
+                }
+                function jump_to_highscore_H(){
+                  load('OPCODE=1&ERROR_CODE=0&DEST=6');
+                }
+                function jump_to_rules(){
+                  load('OPCODE=1&ERROR_CODE=0&DEST=3');
+                }
+                function jump_to_profile(){
+                  load('OPCODE=1&ERROR_CODE=0&DEST=4');
+                }
+                </script>
+                ";
+          echo "
+                <div id='Rules_heading'>HALL OF FAME - AWAY</div>
+                <div id='table'>
+                ";
+          build_away_highscore();
+          echo "
+                </div>
+                <div id='links'>
+                <div id='link0' onclick='jump_to_question()'>Game</div>
+                <div id='link1' onclick='jump_to_profile()'>Profile</div>
+                <div id='link2' onclick='jump_to_rules()'>Rules</div>
+                <div id='link3' onclick='jump_to_highscore_A()'>Highscore-Away</div>
+                <div id='link4' onclick='jump_to_highscore_H()'>Highscore-Home</div>
+                </div>
+                ";
+       }
+       else if($destination==6){         //HIGHSCORE - HOME
+    echo "<script>
+    function jump_to_question(){
+      load('OPCODE=1&ERROR_CODE=0&DEST=2');
+    }
+    function jump_to_highscore_A(){
+      load('OPCODE=1&ERROR_CODE=0&DEST=5');
+    }
+    function jump_to_highscore_H(){
+      load('OPCODE=1&ERROR_CODE=0&DEST=6');
+    }
+    function jump_to_rules(){
+      load('OPCODE=1&ERROR_CODE=0&DEST=3');
+    }
+    function jump_to_profile(){
+      load('OPCODE=1&ERROR_CODE=0&DEST=4');
+    }
+    </script>
+    ";
+    echo "
+    <div id='Rules_heading'>HALL OF FAME - AWAY</div>
+    <div id='table'>
+        ";
+    build_home_highscore();
+        echo "
+    </div>
+    <div id='links'>
+      <div id='link0' onclick='jump_to_question()'>Game</div>
+      <div id='link1' onclick='jump_to_profile()'>Profile</div>
+      <div id='link2' onclick='jump_to_rules()'>Rules</div>
+      <div id='link3' onclick='jump_to_highscore_A()'>Highscore-Away</div>
+      <div id='link4' onclick='jump_to_highscore_H()'>Highscore-Home</div>
+    </div>
+    ";
   }
-
-
-
+  else if($destination==7){
+    echo "
+    <script>
+      function log_in_trigger(){
+        var email=$('#email').text();
+        var password=$('#password').text();
+        load('OPCODE=3&ERROR_CODE=0&TIME=1&EMAIL='+email+'&PASSWORD='+password);
+      }
+      function warp_to_join(){
+        load('OPCODE=1&DEST=8');
+      }
+    </script>
+    ";
+      echo "
+        <div>Email:</div>
+        <div id='email' contenteditable='true'>Email</div>
+        <div>Password:</div>
+        <div id='password' contenteditable='true'>Password</div>
+        <div id='log_in_trigger' onclick='log_in_trigger()'>Log In</div>
+        <div id='join_me_link' onclick='warp_to_join()'>First time? Join here</div>
+      ";
+  }
+  else if($destination==8){
+      echo "
+        <div>Name:</div>
+        <div id='name' contenteditable='true'>Name</div>
+        <div>College:</div>
+        <div id='college' contenteditable='true'>College</div>
+        <div>Email:</div>
+        <div id='email' contenteditable='true'>Email</div>
+        <div> Mobile:</div>
+        <div id='mobile' contenteditable='true'>Mobile</div>
+        <div>Password:</div>
+        <div id='password' contenteditable='true'>Password</div>
+        <div>Confirm Password:</div>
+        <div id='conf_password' contenteditable='true'>Password</div>
+        <div id='join_trigger' onclick='join_trigger()'>Sign Up</div>
+        <div id='log_in_link' onclick='warp_to_login()'>Already a user? Login Here</div>
+      ";
+      echo "
+      <script>
+        function join_trigger(){
+          var name=$('#name').text();
+          var college=$('#college').text();
+          var mobile=$('#mobile').text();
+          var email=$('#email').text();
+          var password=$('#password').text();
+          var conf_password=$('#conf_password').text();
+          if(password!=conf_password){
+            load('OPCODE=1&DEST=8&ERROR_CODE=3');
+          }
+          else{
+              load('OPCODE=3&ERROR_CODE=0&TIME=0&NAME='+name+'&COLLEGE='+college+'&MOBILE='+mobile+'&EMAIL='+email+'&PASSWORD='+password);
+          }
+        }
+        function warp_to_login(){
+          load('OPCODE=1&ERROR_CODE=0&DEST=7');
+        }
+      </script>
+      ";
+  }
+  else{
+    echo "IR Invalid destination";
+  }
+}
+else if($op_code==2){              //////////////////QUESTION MODE
+  $answer=$_POST["ANS"];
+  $cur_level=cur_user_cur_level();
+  $corr_answer=retrieve_answer($cur_level);
+  $yes=5;
+  if($answer==$corr_answer){
+      update_level();
+      $yes=4;
+  }
+  echo "<script>load('OPCODE=1&ERROR_CODE=";
+  echo $yes;
+  echo "&DEST=2');</script>";
+}
+else if($op_code==3){              //USER DETAILS MANAGEMENT
+    $type=$_POST['TIME'];
+    if($type==0){               //JOIN
+        $name=$_POST["NAME"];
+        $college=$_POST["COLLEGE"];
+        $mobile=$_POST["MOBILE"];
+        $email=$_POST["EMAIL"];
+        $password=$_POST["PASSWORD"];
+        $check=check_if_present($email);
+        if($check==1){
+          echo "<script>load('OPCODE=1&ERROR_CODE=2&DEST=8');</script>";
+        }
+        else{
+          insert_user($email,$name,$college,$mobile,$password);
+          load_user($email);
+          echo "<script>
+            load('OPCODE=1&ERROR_CODE=0&DEST=2');
+            </script>";
+        }
+    }
+    else if($type==1){              //SIGN IN
+          $email=$_POST["EMAIL"];
+          $password=$_POST["PASSWORD"];
+          $check=check_if_present($email);
+          if($check==0){
+            echo "<script>throw_error('EMEAIL NOT PRESENT');</script>";
+            echo "<script>load('OPCODE=1&ERROR_CODE=1&DEST=7');</script>";
+          }
+          else{
+            echo "<script>throw_error('EMEAIL PRESENT AUTHENTICATING');</script>";
+            $check=authenticator($email,$password);
+            if($check==0){
+              echo "<script>load('OPCODE=1&ERROR_CODE=1&DEST=7');</script>";
+            }
+            else{
+              load_user($email);
+              echo "<script>load('OPCODE=1&ERROR_CODE=0&DEST=2');</script>";
+            }
+          }
+    }
+    else{
+      echo "IR type=";
+    }
+}
+//ERROR DISPLAY PORTION
+$Error_code=$_POST["ERROR_CODE"];
+//echo "'Error code recieved is "+$Error_code+"')";
+if($Error_code==0){
+    //echo "No errors";
+}
+else if($Error_code==1){
+    echo "<script>throw_error('Invalid Email/Password');</script>";
+}
+else if($Error_code==2){
+    echo "<script>throw_error('Duplicate User Account');</script>";
+}
+else if($Error_code==3){
+    echo "<script>throw_error('Passwords dont match');</script>";
+}
+else if($Error_code==4){
+    echo "<script>throw_error('SUCCESS');</script>";
+}
+else if($Error_code==5){
+    echo "<script>throw_error('FAIL');</script>";
+}
+else{
 
 }
 
 
+//SUPPORT FUNCTIONS
+function cur_user_name(){
+  $email=$_SESSION['EMAIL'];
+  $result=execute_MYSQL("SELECT NAME FROM USERS WHERE EMAIL='$email'");
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        return $row['NAME'];
+    }
+  }
+}
+function cur_user_cur_level(){
+  $email=$_SESSION['EMAIL'];
+    echo "<script>throw_error('Retriving level of $email');</script>";
+  $result=execute_MYSQL("SELECT LEVEL FROM USERS WHERE EMAIL='$email'");
+  if ($result->num_rows > 0) {
+  	// output data of each row
+  	while($row = $result->fetch_assoc()) {
+  			return $row['LEVEL'];
+  	}
+  }
+}
+function cur_user_college(){
+  $email=$_SESSION['EMAIL'];
+  $result=execute_MYSQL("SELECT COLLEGE FROM USERS WHERE EMAIL='$email'");
+  if ($result->num_rows > 0) {
+  	// output data of each row
+  	while($row = $result->fetch_assoc()) {
+  			return $row['COLLEGE'];
+  	}
+  }
+}
+function build_home_highscore(){
+  $result=execute_MYSQL("SELECT NAME,LEVEL FROM USERS WHERE COLLEGE='NITC' ORDER BY LEVEL DESC");
+  $rank=0;
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $name=$row['NAME'];
+      $college=$row['COLLEGE'];
+      $level=$row['LEVEL'];
+      $rank=$rank+1;
+      echo "<div class='row'>
+              <span class='sl_no_col'>$rank</span><span class='name_col'>$name</span><span class='college_col'>$college</span><span class='level_col'>$level</span>
+            </div> ";
+    }
+  }
+}
+function build_away_highscore(){
+  $result=execute_MYSQL("SELECT NAME,COLLEGE,LEVEL FROM USERS ORDER BY LEVEL DESC");
+  $rank=0;
+  if ($result->num_rows > 0) {
+  	while($row = $result->fetch_assoc()) {
+      $name=$row['NAME'];
+      $college=$row['COLLEGE'];
+      $level=$row['LEVEL'];
+      $rank=$rank+1;
+      echo "<div class='row'>
+              <span class='sl_no_col'>$rank</span><span class='name_col'>$name</span><span class='college_col'>$college</span><span class='level_col'>$level</span>
+            </div> ";
+  	}
+  }
+}
+function get_image_name($level){
+  $result=execute_MYSQL("SELECT QUESTION FROM QUESTIONARE WHERE LEVEL=$level");
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        return $row['QUESTION'];
+    }
+  }
+}
+function retrieve_answer($level){
+  $result=execute_MYSQL("SELECT ANSWER FROM QUESTIONARE WHERE LEVEL=$level");
+  if ($result->num_rows > 0) {
+  	while($row = $result->fetch_assoc()) {
+  			return $row['ANSWER'];
+  	}
+  }
+}
+function update_level(){
+  $cur_level=cur_user_cur_level();
+  $cur_level=$cur_level+1;
+  $email=$_SESSION['EMAIL'];
+ $result=execute_MYSQL("UPDATE USERS SET LEVEL=$cur_level WHERE EMAIL='$email'");
+}
+function check_if_present($email){
+  echo "<script>throw_error('Checking if $email is present');</script>";
+  $result=execute_MYSQL("SELECT * FROM USERS WHERE EMAIL='$email'");
+  if ($result->num_rows > 0){
+  	return 1;
+  }
+  else{
+    return 0;
+  }
+}
+function authenticator($email,$password){
+  $result=execute_MYSQL("SELECT PASSWORD FROM USERS WHERE EMAIL='$email'");
+  if ($result->num_rows > 0) {
+    	// output data of each row
+  	while($row = $result->fetch_assoc()) {
+      $r_password=$row['PASSWORD'];
+        //echo "<script>throw_error('Checking if $email is authentic. $password==$r_password');</script>";
+  			if($row['PASSWORD']==$password){
 
-
+  							$_SESSION["EMAIL"]=$email;
+                return 1;
+  			}
+  			else{
+    				return 0;
+  			}
+  	}
+  }
+}
+function load_user($email){
+  session_start();
+  $_SESSION["EMAIL"]=$email;
+}
+//TABLE STRUCTURE
+//EMAIL name  college mobile  password  LEVEL
+function insert_user($email,$name,$college,$mobile,$password){
+  $result=execute_MYSQL("INSERT INTO USERS VALUES('$email','$name','$college',$mobile,'$password',1)");
+}
+function execute_MYSQL($sql){
+  //DATABASE DETAILS
+  $servername="localhost";
+  $username = "root";
+  $password='Amaljose@96';
+  $database= "test";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $database);
+  // Check connection
+  if ($conn->connect_error)
+  {
+     die("Connection failed: " . $conn->connect_error);
+  }
+  //echo "<script>throw_error('SUCCESSFULLY ESTABLISHED CONNECTION');</script>";
+  $result = $conn->query($sql);
+  $conn->close();
+  return $result;
+}
 ?>
