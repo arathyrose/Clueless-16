a5feae2dafb4afd4ea5c36305f5fa26b3e171b7c
Amal Jose <amaljose96@gmail.com>
2016-05-14 14:17:48 +0300

    Finished Home,Login,Rules,Join and updated core.js

--- Login/core.js
+++ Login/core.js
@@ -3,7 +3,9 @@ var client_window_width=1400;
 var main_color="#BFFF00";
 var background_color="#000";
 var scroll_over=true;
-
+function gototathva(){
+  window.location.href="www.tathva.org";
+}
 function update_colors(){
   document.getElementById('content').style.color=main_color;
     document.getElementById('background').style.color=main_color;
@@ -26,11 +28,55 @@ function toggle_HOME_2(){
     scroll_down();
   }
 }
+function callsidebar(){
+  var filterVal = 'blur(4px)';
+  $('#page_data').css('filter',filterVal).css('webkitFilter',filterVal).css('mozFilter',filterVal).css('oFilter',filterVal).css('msFilter',filterVal) .css('transition', 'all 2s ease-out').css('-webkit-transition', 'all 2s ease-out').css('-moz-transition', 'all 2s ease-out').css('-o-transition', 'all 2s ease-out');
+
+  $("#sidebar").animate({
+      left:"0px"
+  },500,function(){});
+}
+function closesidebar(){
+  var filterVal = 'blur(0px)';
+  $('#page_data').css('filter',filterVal).css('webkitFilter',filterVal).css('mozFilter',filterVal).css('oFilter',filterVal).css('msFilter',filterVal) .css('transition', 'all 2s ease-out').css('-webkit-transition', 'all 2s ease-out').css('-moz-transition', 'all 2s ease-out').css('-o-transition', 'all 2s ease-out');
+  $("#sidebar").animate({
+      left:"-"+client_window_width+"px"
+  },500,function(){});
+}
+
+function initsidebar(){
+  document.getElementById("sidebar").style.position="fixed";
+  document.getElementById("sidebar").style.left="-500px";
+  document.getElementById("sidebar").style.top="0px";
+  document.getElementById("sidebar").style.width=client_window_width/5+"px";
+  document.getElementById("sidebar").style.height=client_window_height+"px";
+  document.getElementById("sidebar").style.backgroundColor="#ff0";
+  document.getElementById("sidebar").style.color="#000";
+  document.getElementById("sidebar_trigger_content").style.position="fixed";
+  document.getElementById("sidebar_trigger_content").style.top=client_window_height/8;
+  document.getElementById("sidebar_trigger_content").style.left=client_window_width/25;
+  document.getElementById("sidebar_trigger_content").style.width=client_window_width/30;
+  document.getElementById("sidebar_trigger_content").style.height=client_window_height/18;
+  document.getElementById("sidebar_trigger_sidebar").style.marginTop=client_window_height/8;
+  document.getElementById("sidebar_trigger_sidebar").style.marginLeft=client_window_width/20;
+  document.getElementById("sidebar_trigger_sidebar").style.width=client_window_width/30;
+  document.getElementById("sidebar_trigger_sidebar").style.height=client_window_height/18;
+  document.getElementById("sidebar_trigger_sidebar").style.marginBottom=client_window_height/18;
+  for(var i=0;i<=4;i++){
+    document.getElementById("link"+i).style.fontSize=client_window_height/40;
+    document.getElementById("link"+i).style.marginTop=client_window_height/50;
+    document.getElementById("link"+i).style.marginLeft=client_window_width/20;
+  }
+  document.getElementById("tathva_sidebar").style.position="fixed";
+  document.getElementById("tathva_sidebar").style.left="10px";
+  document.getElementById("tathva_sidebar").style.bottom="10px";
+  document.getElementById("tathva_sidebar").style.width=client_window_width/10+"px";
+  document.getElementById("tathva_sidebar").style.height=client_window_height/7+"px";
+}
 
 
 $("document").ready(function(){
   random_movements();
   update_colors();
   setInterval(function(){update_colors();},1000/60);
-  clean_LOGIN();
 });
---
+++ home.js
@@ -0,0 +1,35 @@
+function clean_HOME(){
+    //SET POSITIONS
+    document.getElementById("Heading").style.fontSize=client_window_height/4;
+    document.getElementById("Heading").style.marginTop=client_window_height/8;
+    document.getElementById("Heading").style.marginLeft=client_window_width/20;
+    document.getElementById("tathva_man").style.width=client_window_width/10;
+    document.getElementById("tathva_man").style.height=client_window_height/7;
+    document.getElementById("tathva").style.left=client_window_width/30;
+    document.getElementById("cpr").style.left=client_window_width/2.7;
+    document.getElementById("HOME_2").style.top=client_window_height*3;
+    document.getElementById("HOME2_Text").style.fontSize=client_window_height/10;
+    document.getElementById("HOME2_Text2").style.fontSize=client_window_height/15;
+    document.getElementById("HOME2_Text").style.marginLeft=client_window_width/10;
+    document.getElementById("HOME2_Text2").style.marginLeft=client_window_width/6;
+    document.getElementById("HOME2_Text2").style.marginTop=client_window_height/15;
+    document.getElementById("HOME2_Text2").style.marginBottom=client_window_height/15;
+    document.getElementById("fb_button").style.marginLeft=client_window_width*2/3;
+
+}
+function scroll_down(){
+  move_background_up();
+
+}
+function scroll_up(){
+  move_background_down();
+
+}
+function toggle_HOME_2(){
+  if(toggle==1){
+    scroll_up();
+  }
+  if(toggle==0){
+    scroll_down();
+  }
+}
